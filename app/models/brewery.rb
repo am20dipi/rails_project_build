4caac84d79cdc1f04a2accfe810339f9249a4332
Adriana <am20dipi@alum.siena.edu>
2021-03-23 15:23:59 -0400

    Model Associations

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,4 @@
 class Brewery < ApplicationRecord
+    belongs_to :user
+    has_many :beers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :breweries
+    has_many :beers, through: :breweries
 end
