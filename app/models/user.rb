e7d614ec8fdec4ed699eaaf663070e05ef817dea
Adriana <am20dipi@alum.siena.edu>
2021-03-25 16:43:05 -0400

    User model Validations, added resources to routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
     has_many :beers, through: :breweries
 
     has_many :comments, through: :breweries
+
+
+    has_secure_password #authenticate, auto validates the password
+    validates :email, presence: true
+    validates :name, presence: true 
 end
+ 
