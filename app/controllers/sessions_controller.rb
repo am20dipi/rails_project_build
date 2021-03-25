fc06b9de4fa68ad5925719e70ae1e5cc305a8a02
Adriana <am20dipi@alum.siena.edu>
2021-03-25 18:13:46 -0400

    Edited create method in SessionsController, edited login form & added flash message capability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,22 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to root_path
     end
+
+    def new 
+        @user = User.new
+    end 
+
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+        # if a user exists AND the user is authenticated via password
+            session[:user_id] == user.id
+
+            redirect_to user_path(user)
+            # user_path(user) is the same as "/users/:id"
+        else
+            flash[:message] = "Sorry, please try again."
+            redirect_to '/login'
+        end
+    end
 end
