0c589c95766970b523ffe295b262938f0dd97a32
Adriana <am20dipi@alum.siena.edu>
2021-03-25 17:22:36 -0400

    Build out create method in Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            # log them in, since they are now signed up
+            session[:user_id] == @user.id
+
+            #redirect to the show page, "@user" is an abstraction of the user_path
+            redirect_to @user 
+        else
+            render :new
+        end
+
     end
 
     def show
@@ -15,6 +26,12 @@ class UsersController < ApplicationController
 
     def update
     end
+
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :email, :password)
+        end
 
 
 end
