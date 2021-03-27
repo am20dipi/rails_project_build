783f7c82089848a06b746c35606dd0809a576d8a
Adriana <am20dipi@alum.siena.edu>
2021-03-27 16:53:21 -0400

    Build out Brewery Controller actions, private method. Build out Brewery show page

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,2 +1,18 @@
 class BreweriesController < ApplicationController
+    before_action :find_brewery, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @breweries = Brewery.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    private
+    def find_brewery
+        @brewery = Brewery.find_by(id: params[:id])
+    end
 end
