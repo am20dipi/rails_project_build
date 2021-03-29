dde8363ec88810259e5d1b0a53c52cc7c2b8d988
Adriana <am20dipi@alum.siena.edu>
2021-03-29 13:32:07 -0400

    Built out private methods and before actions in CommentsController, continuing to build out controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,22 @@
 class CommentsController < ApplicationController
     before_action :redirect_if_not_logged_in
-
+    before_action :find_comment, only: [:show, :edit, :update, :destroy]
+    
     def index 
-
+        @comments = Comment.all
     end
 
     def new
-
+        @comment = Comment.new
     end
 
     def create
-
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            redirect_to @comment
+        else
+            render :new
+        end
     end
 
     def show
@@ -23,5 +29,15 @@ class CommentsController < ApplicationController
 
     def destroy
         
+    end
+
+    private
+
+    def find_comment
+        @comment = Comment.find_by(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content)
     end
 end
