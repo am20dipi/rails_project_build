d85bd06da7046cd33083f7a312d8e49f362fe357
Adriana <am20dipi@alum.siena.edu>
2021-03-29 13:28:08 -0400

    Generated CommentsController, started to add controller actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def index 
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+        
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
+end
