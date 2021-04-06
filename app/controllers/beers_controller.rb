54927284236af50e0fd5856005868fba9849efd6
Adriana <am20dipi@alum.siena.edu>
2021-04-06 15:31:52 -0400

    Built out BeersController controller actions and private methods

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,45 @@
 class BeersController < ApplicationController
+    before_action :find_beer, only: [:show, :edit, :update, :destroy]
+
+    def new 
+       @beer = Beer.new 
+    end
+
+    def show
+    end
+
+    def create 
+        @beer = Beer.create(beer_params)
+        if @beer.save
+            redirect_to @brewery
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @beer.update(beer_params) 
+        if @beer.save
+            redirect_to @brewery
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @beer.destroy
+        redirect_to @brewery, :notice => "Successfully deleted."
+    end
+
+    private
+    def find_beer
+        @beer = Beer.find_by(id: params[:id])
+    end
+
+    def beer_params
+        params.require(:beer).permit(:name, :type, :abv, :description)
+    end
 end
